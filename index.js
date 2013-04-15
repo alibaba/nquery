e7addcc22e6127c6e3459286f03fc722ca3e32cb
windyrobin <windyrobin@Gmail.com>
2013-04-15 14:51:13 +0800

    change remove the old  interfaces

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 exports.parse         = require('./lib/parser').parse;
 exports.tplParse      = require('./lib/parser').tplParse;
 
-//compatible with old name
-exports.AstWrapper    = require('./lib/ast_helper').Reader;
-exports.toSQL         = require('./lib/adapter').toSQL;
-exports.exprToSQL     = require('./lib/adapter').exprToSQL;
-
-exports.getRefColumns = require('./lib/ast_helper').getRefColumns;
-exports.getSelRefCols = require('./lib/ast_helper').getSelRefCols;
-
-
 exports.Table         = require('./lib/table');
 exports.Query         = require('./lib/query');
 exports.Parser        = require('./lib/parser');
