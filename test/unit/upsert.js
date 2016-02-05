7fefef17b513bc2890a1de3428b31ab00be48999
Robert Plummer <robertleeplummerjr@gmail.com>
2016-02-05 15:33:47 -0500

    provide back ticks in adapter provide a means of upserting aout of adapter provide tests ensuring it works

--- test/unit/upsert.js
+++ test/unit/upsert.js
@@ -1,12 +1,13 @@
 var should = require('should');
 var Parser = require('../../lib/parser');
+var Adapter = require('../../lib/adapter');
 
 function inspect(obj) {
   console.log(require('util').inspect(obj, false, 10, true));
 }
 
 describe('upsert test',function(){
-  it('insert test', function() {
+  it('should parse', function() {
     var sql, ast;
 
     sql = "INSERT INTO `user_info` SET `last_login_time` = '2012-12-18 12:44:21', `last_login_ip` = 'hohoo' ON duplicate KEY UPDATE `last_login_time` = '2012-12-18 12:44:21', `last_login_ip` = 'hohoo'";
@@ -39,6 +40,18 @@ describe('upsert test',function(){
         }
       ]
     });
+  });
+});
+
+describe('upsert test',function(){
+  it('should be able to adapt out', function() {
+    var sql, ast;
+
+    sql = "INSERT INTO `user_info` SET `last_login_time` = '2012-12-18 12:44:21', `last_login_ip` = 'hohoo' ON DUPLICATE KEY UPDATE `last_login_time` = '2012-12-18 12:44:21', `last_login_ip` = 'hohoo'";
+
+    ast = Parser.parse(sql);
+
+    Adapter.toSQL(ast, { back_ticks: true }).should.eql(sql);
   });
 });
 
